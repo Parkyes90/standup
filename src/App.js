a046252e42e0ccf519a839efe56a15cc18af5ea4
parkyes90 <parkyes90@gmail.com>
2018-03-15 15:55:00 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo(cooltext2.png';
 import './App.css';
+import Editor from './Editor'
 
 class App extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        console.log(this, e);
+    }
+    isAnonymous() {
+        return true;
+    }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Stand Up 앱을 만들어 봅시다.</h2>
         </header>
-        <p className="App-intro">
-          프로젝트 시작
-        </p>
+        <Editor {...this}/>
       </div>
+
     );
   }
 }
